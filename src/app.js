d50106ec6945b5a34839fec2c448a582a5bd8e13
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-02-03 15:22:12 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -8,22 +8,14 @@ import cookieParser from 'cookie-parser';
  * @returns {Object} The initialized application object.
  */
 
-// export const init = () => {
-//   const app = express();
+export const init = () => {
+  const app = express();
 
-//   app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-//   app.use(express.json({ limit: '16kb' }));
-//   app.use(express.urlencoded({ extended: true }));
-//   app.use(express.static('public'));
-//   app.use(cookieParser());
+  app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+  app.use(express.json({ limit: '16kb' }));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(cookieParser());
 
-//   return app;
-// };
-
-const app = express();
-
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(cookieParser());
+  return app;
+};
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@
  */
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { init } from './app.js';
 
 dotenv.config({
   path: '.env',
 });
 
 connectDB();
+init();
