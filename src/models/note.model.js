c39e407ce051cb73a3186f615600b11e4bb05ae6
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-02-03 14:18:59 +0530

    Add mongoose-aggregate-paginate-v2 package

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 /**
  * Represents a note in the system.
  * @typedef {Object} Note
