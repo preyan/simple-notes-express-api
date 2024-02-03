52abdb39ef9d5dfb62be22acf24667c2286cc888
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-02-04 01:28:30 +0530

    Add login functionality and access token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,25 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
 /**
+ * Generates access and refresh tokens for a given user ID.
+ * @param {string} userId - The ID of the user.
+ * @returns {Promise<{accessToken: string, refreshToken: string}>} - The generated access and refresh tokens.
+ * @throws {ApiError} - If access token generation fails.
+ */
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const refreshToken = User.generateRefreshToken();
+    const accessToken = User.generateAccessToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false }); //This is to avoid the pre-save middleware from running during the update operation
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, 'Access token generation failed');
+  }
+};
+
+/**
  * Registers a user.
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
@@ -70,4 +89,54 @@ const registerUser = asyncHandler(async (req, res, next) => {
     .json(new ApiResponse(201, createdUser, 'User registered successfully'));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res, next) => {
+  //Get the user data from the request body
+  const { email, username, password } = req.body;
+
+  //Check if any field is empty
+  if (!email || !username) {
+    throw new ApiError(400, 'Username or email is required');
+  }
+  if (!password) {
+    throw new ApiError(400, 'Password is required');
+  }
+
+  //Find the user in the database
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, 'User not found');
+  }
+
+  //Check if the password is correct
+  const isPasswordMatch = await user.isPasswordCorrect(password); // `user` is an instance of the User model, (Ref: Line 86)
+
+  if (!isPasswordMatch) {
+    throw new ApiError(401, 'Invalid password');
+  }
+
+  //Generate access and refresh tokens for a given user ID
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  //!IMPORTANT : Judge if query is stressing the server, if yes do it the user object itself, else directly update the database
+  const loggedInUser = await User.findById(user._id).select(
+    '-password -refreshToken'
+  );
+  const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+
+  //Creates a new user, sets refreshToken and accessToken is cookies and return a success message
+  res
+    .status(200)
+    .cookie('refreshToken', refreshToken, cookieOptions)
+    .cookie('accessToken', accessToken, cookieOptions)
+    .json(new ApiResponse(200, user, 'User logged in successfully'));
+});
+
+export { registerUser, loginUser };
