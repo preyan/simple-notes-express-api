d542c3c81384649d7c9df39669051adba54a4181
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-02-04 01:55:10 +0530

    Add login and logout routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUser, registerUser } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
+import { verifyJWTToken } from '../middlewares/auth.middleware.js';
 
 /**
  * Express router instance for handling user routes.
@@ -9,5 +10,9 @@ import { upload } from '../middlewares/multer.middleware.js';
 const router = Router();
 
 router.route('/register').post(upload.single('avatar'), registerUser);
+router.route('login').post(loginUser);
+
+//Secured routes
+router.route('logout').post(verifyJWTToken, logoutUser);
 
 export default router;
